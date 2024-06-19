8b467a938c9466215e78a92f128fe98227d6ac32
Oleksandr Pluzhnykov <bbynnas@yahoo.com>
2024-06-20 00:10:13 +0300

    Update ContactsPage.jsx

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,11 +1,14 @@
 import { Container, Box, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { selectIsLoading } from "../redux/contacts/selectors";
+import { fetchContacts } from "../redux/contacts/operations";
 import ContactForm from "../components/ContactForm/ContactForm";
 import SearchBox from "../components/SearchBox/SearchBox";
 import ContactList from "../components/ContactList/ContactList";
 
 const ContactsPage = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
   const isLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
@@ -20,6 +23,7 @@ const ContactsPage = () => {
         </Typography>
         <ContactForm />
         <SearchBox />
+        {isLoading && <p>Loading...</p>}
         <ContactList />
       </Box>
     </Container>
